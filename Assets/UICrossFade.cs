a9e5bb00b2685bda6240e6e87668fa622c60876d
Nicholas Liang <sss021418@gmail.com>
2021-01-31 06:53:51 -0800

    start screen animation

---
+++ Assets/UICrossFade.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICrossFade : MonoBehaviour
+{
+    public float startTime;
+    public float duration;
+    private Image panel;
+    void Start()
+    {
+        panel = GetComponent<Image>();
+        StartCoroutine(fade(startTime, duration));
+    }
+
+    private IEnumerator fade(float startTime, float duration)
+    {
+        yield return new WaitForSeconds(startTime);
+        panel.GetComponent<Image>().CrossFadeAlpha(0f, duration, false);
+        yield return null;
+    }
+}
