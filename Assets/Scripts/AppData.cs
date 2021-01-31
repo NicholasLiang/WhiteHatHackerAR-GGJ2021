f41a696bc83d561663eda31bd7476186615387f6
Nicholas Liang <sss021418@gmail.com>
2021-01-31 11:38:04 -0800

    fix assets is not moving with house

---
+++ Assets/Scripts/AppData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppData : MonoBehaviour
+{
+    public static AppData appdata;
+
+    public float someExampleVariable;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        if (appdata == null)
+        {
+            appdata = this;
+        }
+    }
+}
