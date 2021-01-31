26e240409a36f467f23e56861b470eab35e71521
Nicholas Liang <sss021418@gmail.com>
2021-01-31 15:19:23 -0800

    add cross scene data transfer

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -2,19 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AppData : MonoBehaviour
 {
-    public static AppData appdata;
+    public bool initialized;
 
-    public float someExampleVariable;
+    public bool camera1Touched;
+    
+    public bool camera2Touched;
+    
 
-    void Awake()
+    public bool KimFamilyHacked;
+
+    public bool ChenFamilyHacked;
+
+    public bool SimpsonsFamilyHacked;
+
+    void Awake() 
     {
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(this);
 
-        if (appdata == null)
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("AppData");
+
+        if (objs.Length > 1)
         {
-            appdata = this;
+            foreach (var obj in objs)
+            {
+                if (obj.GetComponent<AppData>().initialized == false)
+                {
+                    Destroy(obj);
+                }
+             }
         }
+
+        initialized = true;
+
     }
+
+
+    public void KimFailyHackedBool(bool value)
+    {
+        KimFamilyHacked = value;
+    }
+    public void ChenFailyHackedBool(bool value)
+    {
+        ChenFamilyHacked = value;
+    }
+    public void SimpsonsFailyHackedBool(bool value)
+    {
+        SimpsonsFamilyHacked = value;
+    }
+
+    public void Camera1TouchedBool(bool value)
+    {
+        camera1Touched = value;
+    }
+    public void Camera2TouchedBool(bool value)
+    {
+        camera2Touched = value;
+    }
+
+
 }
---
+++ Assets/Scripts/LoadSceneWhenStart.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class LoadSceneWhenStart : MonoBehaviour
+{
+    private AppData appData;
+    public UnityEvent onCamera1Touched;
+    public UnityEvent onCamera2Touched;
+    public UnityEvent onKimFamilyWifiHacked;
+    public UnityEvent onChenFamilyWifiHacked;
+    public UnityEvent onSimpsonsFamilyWifiHacked;
+
+    void Start()
+    {
+        appData = (AppData)GameObject.Find("AppData").GetComponent<AppData>();
+        LoadScene();
+    }
+
+    public void KimFamilyWifiHackedEvent()
+    {
+        onKimFamilyWifiHacked.Invoke();
+    }
+    public void ChenFamilyWifiHackedEvent()
+    {
+        onChenFamilyWifiHacked.Invoke();
+    }
+    public void SimpsonsFamilyWifiHackedEvent()
+    {
+        onSimpsonsFamilyWifiHacked.Invoke();
+    }
+    public void Camera1TouchedEvent()
+    {
+        onCamera1Touched.Invoke();
+    }
+    public void Camera2TouchedEvent()
+    {
+        onCamera2Touched.Invoke();
+    }
+    public void LoadScene()
+    {
+        if (appData.camera1Touched)
+        {
+            Camera1TouchedEvent();
+        }
+        if (appData.camera2Touched)
+        {
+            Camera2TouchedEvent();
+        }
+        if (appData.KimFamilyHacked)
+        {
+            KimFamilyWifiHackedEvent();
+        }
+    }
+}
