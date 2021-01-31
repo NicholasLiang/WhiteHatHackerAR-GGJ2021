f7624aa47318f55dea4075132c6e7afb938d10d3
Nicholas Liang <sss021418@gmail.com>
2021-01-31 04:30:18 -0800

    menu finished

---
+++ Assets/BlockView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BlockView : MonoBehaviour
+{
+    public UnityEvent OnBlockViewEnter;
+    public UnityEvent OnBlockViewExit;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            OnBlockViewEnter.Invoke();
+        }
+    }
+
+    //When the Primitive exits the collision, it will change Color
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            OnBlockViewExit.Invoke();
+        }
+    }
+}
